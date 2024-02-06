1fb2b530be0fc5d8427a3016a4f676e56636d209
GarrawayM <ma360970@ucf.edu>
2024-02-05 20:46:10 -0500

    re-updated exercises

--- exercises.js
+++ exercises.js
@@ -1,11 +1,11 @@
-//Flattening
+//3-1 Flattening
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
 let flattened = arrays.flat();
 console.log(flattened);
 // → [1, 2, 3, 4, 5, 6]
 
-//Your own loop
+//3-2 Your own loop
 // Your code here
 function loop(value, test, update, body) {
     
@@ -22,47 +22,36 @@ loop(3, n => n > 0, n => n - 1, console.log);
 // → 1
 
 
-//Everything
-function every(array, test) {
-    // Your code here.
+//3-3 EveryLoop
+//3-4 EverySome
+
+function everyLoop(array, test) {
+   
     for (let element of array) {
+      
+      if (!test(element)) {
         
-        if (!test(element)) {
-         
-          return false;
-        }
+        return false;
       }
-      return true;
+    }
+    return true;
   }
   
-  console.log(every([1, 3, 5], n => n < 10));
-  // → true
-  console.log(every([2, 4, 16], n => n < 10));
-  // → false
-  console.log(every([], n => n < 10));
-  // → true
-
-
-  //Dominant writing direction
-  function dominantDirection(text) {
-    // Your code here.
-    let scripts = countBy(text, char => {
-        
-        let script = characterScript(char.codePointAt(0));
-        
-        return script ? script.name : null;
-      });
-      
-      scripts = scripts.filter(({name}) => name != null);
-      
-      if (scripts.length == 0) return "ltr";
-      
-      let dominantScript = scripts.reduce((a, b) => a.count > b.count ? a : b);
-      
-      return dominantScript.name == "Arabic" ? "rtl" : "ltr";
-    }
+  function everySome(array, test) {
     
-  console.log(dominantDirection("Hello!"));
-  // → ltr
-  console.log(dominantDirection("Hey, مساء الخير"));
-  // → rtl
+    return !array.some(element => !test(element));
+  }
+  
+ 
+  console.log(everyLoop([1, 3, 5], n => n < 10));
+  // → true
+  console.log(everyLoop([2, 4, 16], n => n < 10));
+  // → false
+  console.log(everyLoop([], n => n < 10));
+  // → true
+  console.log(everySome([1, 3, 5], n => n < 10));
+  // → true
+  console.log(everySome([2, 4, 16], n => n < 10));
+  // → false
+  console.log(everySome([], n => n < 10));
+  // → true
