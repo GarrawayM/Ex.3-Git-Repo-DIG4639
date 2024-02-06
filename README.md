103b7d4d0e0547952580f9ea5f9cbb4b62ada719
GarrawayM <ma360970@ucf.edu>
2024-02-05 19:59:48 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Ex.3-Git-Repo-DIG4639
+Have courage and be kind. -Cinderella
--- node.js
+++ node.js
@@ -1,1 +1,9 @@
-
+var quotes = [
+    "Be the change that you wish to see in the world. - Mahatma Gandhi",
+    "Don't cry because it's over, smile because it happened. - Dr. Seuss",
+    "Have courage and be kind. -Cinderella" 
+  ];
+  
+  var index = Math.floor(Math.random() * 3);
+  
+  console.log(quotes[index]);
